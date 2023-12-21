4a4929e3caa62390ed2d88fe7c9b64a366bfc423
Cecile Duffaut <cduffaut@c1r10s4.42lausanne.ch>
2023-12-21 11:38:23 +0100

    Auto push

--- utils/texture_in_data.c
+++ utils/texture_in_data.c
@@ -6,34 +6,34 @@
 /*   By: cduffaut <cduffaut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/21 10:19:15 by cduffaut          #+#    #+#             */
-/*   Updated: 2023/12/21 11:36:21 by cduffaut         ###   ########.fr       */
+/*   Updated: 2023/12/21 11:38:08 by cduffaut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../Includes/cub3d.h"
 
-static int	put_texture_in_node(t_pixput texture, t_input *input, char *name)
+static int	put_texture_in_node(t_pixput *texture, t_input *input, char *name)
 {
-	texture.img =  mlx_xpm_file_to_image(input->mlx, name, &texture.width, \
-		&texture.heigth);
-	if (!texture.img)
+	texture->img =  mlx_xpm_file_to_image(input->mlx, name, &texture->width, \
+		&texture->heigth);
+	if (!texture->img)
 		return (1);
-	texture.addr = mlx_get_data_addr(texture.img, &texture.bit_pp, \
-		&texture.line_len, &texture.endian);
-	if (!texture.addr)
+	texture->addr = mlx_get_data_addr(texture->img, &texture->bit_pp, \
+		&texture->line_len, &texture->endian);
+	if (!texture->addr)
 		return (1);
 	return (0);
 }
 
 int	all_texture_in_data(t_input *input)
 {
-	if (put_texture_in_node(input->t_no, input, input->no) == 1);
+	if (put_texture_in_node(&input->t_no, input, input->no) == 1);
 		return (1);
-	if (put_texture_in_node(input->t_so, input, input->so) == 1);
+	if (put_texture_in_node(&input->t_so, input, input->so) == 1);
 		return (1);
-	if (put_texture_in_node(input->t_we, input, input->we) == 1);
+	if (put_texture_in_node(&input->t_we, input, input->we) == 1);
 		return (1);
-	if (put_texture_in_node(input->t_ea, input, input->ea) == 1);
+	if (put_texture_in_node(&input->t_ea, input, input->ea) == 1);
 		return (1);
 	return (0);
 }
