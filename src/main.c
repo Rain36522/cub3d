f6866a7a50a06f1d02192c52131790ea76e053c4
Cecile Duffaut <cduffaut@c1r14s5.42lausanne.ch>
2024-01-05 08:52:54 +0100

    Auto push

--- src/main.c
+++ src/main.c
@@ -77,7 +77,7 @@ int	main(int argc, char **argv)
 
 	if (argc != 2 )
 		printf ("Error\nToo much arguments in input.\n");
-	(check_extension(argv[1]) != 0)
+	if (check_extension(argv[1]) != 0)
 		exit(1);
 	data = init_game(argv);
 	if (!data)
